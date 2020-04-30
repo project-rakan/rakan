d3b0e5d0af1243be9139bfd9ab8fe6f333bc4669
Idess Lee <idess@uw.edu>
2020-04-29 18:02:05 -0700

    Added rough design in Graph.h + Node.h

--- src/Graph.h
+++ src/Graph.h
@@ -1,10 +1,94 @@
 #ifndef GRAPH_H_
 #define GRAPH_H_
 
+#include <inttypes.h>      // for uint32_t
+#include <stdio.h>         // for FILE *
+#include <unordered_map>   // for std::unordered_map
+#include <vector>          // for std::vector
+
+using std::unordered_map;
+using std::vector;
+
 namespace rakan {
 
 class Graph {
+ public:
+  // Default constructor.
+  Graph();
 
+  // Default destructor. Also destructs nodes on this graph.
+  ~Graph();
+
+  // Loads the graph from the given file if supplied. If not,
+  // loads an empty graph.
+  //
+  // Arguments:
+  //  - num_districts: the maximum number of districts this graph
+  //                   can have
+  //  - file: a path to the index file to load from; if nullptr,
+  //          loads an empty graph
+  //
+  // Returns:
+  //  - true iff loading successful
+  //  - false otherwise
+  bool LoadGraph(uint32_t num_districts, FILE *file = nullptr);
+
+  // Adds a node to this graph.
+  //
+  // Arguments:
+  //  - node: the node to add
+  //
+  // Returns:
+  //  - true iff adding node successful
+  //  - false if node already exists
+  bool AddNode(Node& node);
+
+  // Adds an edge between the two supplied nodes. Both nodes
+  // must exist on the graph.
+  //
+  // Arguments:
+  //  - node1: the neighbor of node2
+  //  - node2: the neighbor of node1
+  //
+  // Returns:
+  //  - true iff adding edge successful
+  //  - false if the edge already exists
+  bool AddEdge(Node& node1, Node& node2);
+
+  // Returns a vector of all nodes on this graph.
+  vector<Node>& get_nodes() { return nodes_; }
+
+  // Mutators for metric parameters.
+  //
+  // Arguments:
+  //  - val: the value to set the corresponding metric
+  //         parameter to
+  void set_alpha(int val) { alpha_ = val; }
+  void set_beta(int val) { beta_ = val; }
+  void set_gamma(int val) { gamma_ = val; }
+  void set_eta(int val) { eta_ = val; }
+
+ private:
+  // A vector of all the nodes on this graph.
+  vector<Node> nodes_;
+
+  // A map of the district ID to the nodes in it.
+  unordered_map<int, int> nodes_in_district_;
+
+  // A map of the district ID to the nodes on its perimeter.
+  unordered_map<int, int> nodes_on_perim_;
+
+  // A map of the district ID to another map of the nodes
+  // on the perimeter of that district to the list of neighbors
+  // in another district.
+  unordered_map<int, unordered_map<int, vector<int>>>
+                                    perim_nodes_to_neighbors_;
+
+  // Metric parameters.
+  double alpha_;
+  double beta_;
+  double gamma_;
+  double eta_;
 };        // class Graph
 
 }         // namespace rakan
--- src/Node.h
+++ src/Node.h
@@ -1,10 +1,61 @@
 #ifndef NODE_H_
 #define NODE_H_
 
+#include <string>           // for std::string
+#include <unordered_map>    // for std::unordered_map
+#include <vector>           // for std::vector
+
+using std::string;
+using std::unordered_map;
+using std::vector;
+
 namespace rakan {
 
 class Node {
+ public:
+  // Default constructor.
+  Node();
 
+  // Constructor with a unique ID.
+  Node(int id);
+
+  // Constructor with a unique ID and a district ID.
+  Node(int id, int district);
+
+  // Constructor with a unique ID, a district ID, and a list
+  // of neighbors.
+  Node(int id, int district, vector<int>& neighbors);
+
+  // Destructor.
+  ~Node();
+
+  // Adds a neighbor to this node. A neighbor relationship
+  // is bi-directional.
+  //
+  // Arguments:
+  //  - other: the neighbor to add, also adds this node to
+  //           the other's list of neighbors
+  //
+  // Returns:
+  //  - true iff the relationship is new
+  //  - false otherwise
+  bool AddNeighbor(Node& other);
+
+  // Returns the district this node resides in.
+  int get_district() { return district_; }
+
+ private: 
+  // The unique node ID.
+  int id_;
+
+  // The district this node resides in.
+  int district_;
+
+  // The list of neighbors this node has.
+  vector<int> neighbors_;
+
+  // A demographics map.
+  unordered_map<string, int> demographics;
 };        // class Node
 
 }         // namespace rakan
