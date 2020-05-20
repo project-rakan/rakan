8df1ff267826adcea7257a1231bde850cb4ef16c
Norton Pengra <npengra317@gmail.com>
2020-05-19 18:43:34 -0700

    include a dummy (gitignored) secrets file

---
+++ configs/.secrets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export DEBUG_MODE=false
+export DATABASE_USER="default_user"
+export DATABASE_PASS="p@ssword1"
+export DATABASE_TABLE="table1"
+export DJANGO_SECRET="nevergonnagiveyouup"
