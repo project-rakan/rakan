2cc0b46560b2d6a3b1aad1098a1867fc26fe02ed
kdmcc <kdmcc@uw.edu>
2020-05-21 15:45:00 -0700

    Mock engine class for rest api testing

--- rakan/cRakan/wrapper.pyx
+++ rakan/cRakan/wrapper.pyx
@@ -2,7 +2,7 @@
 
 from cython.operator import dereference, preincrement, address
 
-from libcpp.string cimport string as cstring
+from libcpp.vector cimport vector as cvector
 
 from wrapper cimport Runner as cRunner
 from wrapper cimport Graph as cGraph
@@ -10,35 +10,40 @@ from wrapper cimport Graph as cGraph
 cdef class Engine:
     cdef cRunner* _runner
     cdef cGraph* _graph
+    cpdef int districts
+    cpdef int precincts
 
-    def __cinit__(self):
+    def __cinit__(self, int districts, int precincts):
+        print('mock init')
         self._runner = new cRunner()
+        self.districts = districts
+        self.precincts = precincts
 
     def __dealloc__(self):
         del self._runner
 
     def addNode(self, int id, int county, int minorityPopulation, int majorityPoplation):
-        print('addNode')
+        print('mock addNode')
         print(id, county, minorityPopulation, majorityPoplation)
 
     def addEdge(self, int id1, int id2):
-        print('addEdge')
+        print('mock addEdge')
 
-    def setDistricts(int districts):
-        print('setDistricts')
-
-    def populate(self):
-        print('populate')
+    def setDistricts(self, cvector[int] districts):
+        print('mock setDistricts')
 
     def getMaps(self):
-        print('getMaps')
+        print('mock getMaps')
+        return [[self.districts - 1] * self.precincts]
 
     def getScores(self):
-        print('getScores')
+        print('mock getScores')
+        return[{'compactness': 0.1, 'vra': 0.8, 'population':0.6, 'political': 0.76}]
 
     def seed(self):
-        print('seed')
+        print('mock seed')
 
     def walk(int stepsToTake, double alpha, double beta, double gamma, double eta):
-        print('walk')
+        print('mock walk')
+
 # dereference(self._runner)
