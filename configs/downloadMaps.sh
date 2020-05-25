c86ea5904d3d70797571a56344dd90854676bc14
Norton Pengra <npengra317@gmail.com>
2020-05-25 16:18:11 -0700

    download bladecaller maps

---
+++ configs/downloadMaps.sh
@@ -0,0 +1,9 @@
+# Iowa
+
+wget https://github.com/project-rakan/bladecaller/blob/master/output/iowa/iowa.districts.json?raw=true -O /var/www/html/stateinfo/IA.districts.json
+wget https://github.com/project-rakan/bladecaller/blob/master/output/iowa/iowa.json?raw=true -O /var/www/html/stateinfo/IA.json
+
+# Washington
+
+wget https://github.com/project-rakan/bladecaller/blob/master/output/washington/washington.districts.json?raw=true -O /var/www/html/stateinfo/WA.districts.json
+wget https://github.com/project-rakan/bladecaller/blob/master/output/washington/washington.json?raw=true -O /var/www/html/stateinfo/WA.json
