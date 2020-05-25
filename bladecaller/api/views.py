5a6ed1166fca3c30a7ba81d4e16601454a3afd38
Norton Pengra <npengra317@gmail.com>
2020-05-25 15:36:14 -0700

    rebuild createGUID api view

--- bladecaller/api/views.py
+++ bladecaller/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from uuid import uuid4
 
 # Create your views here.
+
+# API to handle:
+
+"""
+Bladecaller
+# CreateGuid Request/Response
+# GetDistricting Request/Response
+# GetStateInfo Request/Response
+
+Rakan
+# ScoreMap (Request/Response)
+# StartMapJob (Request/Update)
+"""
+
+@api_view(['POST'])
+def createGuid(request):
+    if request.method == 'POST':
+        state = request.data.get('state', 'XX').lower()
+        jobType = request.data.get('jobType', 'XX').lower()
+        uuid = uuid4()
+        return Response({"guid": f"{state}-{jobType}-{str(uuid)}"})
+    return Response({"data": None})
