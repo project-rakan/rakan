050372a12fe41e971156a9c8e46d9c4751863692
Norton Pengra <npengra317@gmail.com>
2020-05-25 19:58:49 -0700

    Create view fro GUID + districting

--- bladecaller/api/views.py
+++ bladecaller/api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
+
+from api import models
 
 from uuid import uuid4
 
@@ -9,9 +12,7 @@ from uuid import uuid4
 
 """
 Bladecaller
-# CreateGuid Request/Response
 # GetDistricting Request/Response
-# GetStateInfo Request/Response
 
 Rakan
 # ScoreMap (Request/Response)
@@ -20,9 +21,28 @@ Rakan
 
 @api_view(['POST'])
 def createGuid(request):
-    if request.method == 'POST':
-        state = request.data.get('state', 'XX').lower()
-        jobType = request.data.get('jobType', 'XX').lower()
-        uuid = uuid4()
-        return Response({"guid": f"{state}-{jobType}-{str(uuid)}"})
-    return Response({"data": None})
+    state = request.data.get('state', 'XX').lower()
+    jobType = request.data.get('jobType', 'XX').lower()
+    uuid = uuid4()
+    return Response({"guid": f"{state}-{jobType}-{str(uuid)}"})
+
+@api_view(['POST'])
+def getDistricting(request):
+    # Check fields are there
+    for field in ['state', 'mapId']:
+        if field not in request.data:
+            return Response({'msg': 'Missing a field', 'missing_field': field}, status=status.HTTP_400_BAD_REQUEST)
+
+    mapId = request.data['mapId']
+    state = request.data['state']
+
+    # Check mapID is there
+    if len(models.GeneratedMap.objects.filter(id=mapId, state=state)) == 0:
+        return Response({'msg': 'Unable to find mapId', mapId: mapId, state: state}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Return the map
+    return Response({
+        'state': request.data['state'],
+        'map': [[precinct_id, district_id] for precinct_id, district_id in enumerate(models.GeneratedMap.objects.get(id=mapId, state=state).mapContents)]
+    })
+    
