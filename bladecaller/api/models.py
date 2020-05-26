daf9e687fe6b2266c03c255b56d9e5959a772319
Norton Pengra <npengra317@gmail.com>
2020-05-26 15:14:04 -0700

    create signal handler for job creation

--- bladecaller/api/models.py
+++ bladecaller/api/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from multiprocessing import Process
+from rakan import Engine
+
+import os
+from bladecaller.settings import MAP_ROOT
 
 # Create your models here.
 
@@ -49,3 +57,32 @@ class Job(models.Model):
 
     def __str__(self):
         return self.jobId
+
+
+@receiver(post_save, sender=Job)
+def queue_new_job(sender, **kwargs):
+    job = kwargs.get('instance')
+    # start an engine in a different process
+
+    def task(job):
+        engine = engine(job.state.stateJsonLocation)
+        engine.walk(job.steps, job.alpha, job.beta, job.gamma, job.eta)
+
+        maps = engine.getMaps()
+        scores = engine.getScores()
+
+        for map_, score_ in zip(maps, scores):
+            mapModel = GeneratedMap.objects.create(
+                state=job.state,
+                mapContents=map_,
+                compactness=score_['compactness'],
+                vra=score_['vra'],
+                population=score_['population'],
+                borderRespect=score_['political'],
+            )
+            job.generatedMaps.add(mapModel)
+    
+    process = Process(target=task, args=(job,))
+    process.start()
+    process.join()
+
