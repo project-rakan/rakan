b834a254e4511d265ca1215c2a0549f260435a59
Norton Pengra <npengra317@gmail.com>
2020-05-25 19:57:58 -0700

    Add Job and Map models

--- bladecaller/api/models.py
+++ bladecaller/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class GeneratedMap(models.Model):
+    state = models.CharField(max_length=2)
+
+    mapContents = ArrayField(
+        models.IntegerField(),
+    )
+
+    compactness = models.FloatField()
+    distribution = models.FloatField()
+    borderRespect = models.FloatField()
+    vra = models.FloatField()
+
+
+class Job(models.Model):
+    type = models.CharField(max_length=256, choices=(
+        ('score', 'score'),
+        ('walk', 'walk'),
+    ))
+
+    maxSteps = models.IntegerField(default=200)
+    jobId = models.CharField(max_length=256, unique=True)
+
+    alpha = models.FloatField()
+    beta = models.FloatField()
+    gamma = models.FloatField()
+    eta = models.FloatField()
