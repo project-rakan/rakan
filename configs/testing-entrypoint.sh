b2ade8bc45e5f48f6ba6f47758f11339a5b8ac76
Norton Pengra <npengra317@gmail.com>
2020-05-27 13:50:38 -0700

    update testing endpoints

--- configs/testing-entrypoint.sh
+++ configs/testing-entrypoint.sh
@@ -1,4 +1,39 @@
 #!/bin/sh -l
+. /home/project/configs/.secrets.sh
+
+# launch postgres using values in . .secrets.sh
+service postgresql start
+
+# launch rabbitmq
+rabbitmq-server -detached
+
+# insert credentials into the database if it hasn't already
+su - postgres -c "createdb $DATABASE_TABLE"
+su - postgres -c "psql -c \"CREATE USER $DATABASE_USER with encrypted password '$DATABASE_PASS'\""
+su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE $DATABASE_TABLE TO $DATABASE_USER;\""
+su - postgres -c "psql -c \"ALTER USER $DATABASE_USER WITH SUPERUSER;\""
+
+# Start a build for the celery worker
+make build
+
+# Create migrations and whatnot
+cd bladecaller
+python3.7 manage.py makemigrations
+python3.7 manage.py migrate
+python3.7 manage.py collectstatic --noinput
+cd ..
+
+# run nginx if it isn't running already
+service nginx start
+
+# Configure rabbit
+rabbitmqctl add_user $RABBIT_USER $RABBIT_PASS
+rabbitmqctl add_vhost $RABBIT_VHOST
+
+# Launch celery
+cd bladecaller
+( celery -A bladecaller worker > $RAKAN_LOCATION/logs/celery.log ) &
+cd ..
 
 make build
 
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 pip:
 	python3.7 -m pip install -r requirements.txt
 
-build: rakan/cRakan
-	cd rakan && python3.7 setup.py build_ext --inplace
-
 cmake: rakan/cRakan
 	cd rakan/cRakan/build && cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" && make all
+
+build: cmake
+	cd rakan && python3.7 setup.py build_ext --inplace
 
 clean:
 	cd rakan && rm -rf *.so  cRakan/wrapper.cpp tests/*.so build
