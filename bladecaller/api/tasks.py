fa0f1a52d2bf52a2c137241db4c60e4bb7dcc535
Norton Pengra <npengra317@gmail.com>
2020-05-27 12:20:16 -0700

    create celery run task

---
+++ bladecaller/api/tasks.py
@@ -0,0 +1,26 @@
+from celery import shared_task
+from api.models import Job, GeneratedMap
+from rakan import Engine
+
+@shared_task
+def performMetropolisHastingsWalk(jobId: int):
+    job = Job.objects.get(jobId=jobId)
+    engine = Engine(job.state.stateJsonLocation)
+    engine.walk(job.steps, job.alpha, job.beta, job.gamma, job.eta)
+
+    maps = engine.getMaps()
+    scores = engine.getScores()
+
+    for map_, score_ in zip(maps, scores):
+        mapModel = GeneratedMap.objects.create(
+            state=job.state,
+            mapContents=map_,
+            compactness=score_['compactness'],
+            vra=score_['vra'],
+            distribution=score_['population'],
+            borderRespect=score_['political'],
+        )
+        job.generatedMaps.add(mapModel)
+
+    job.finished = True
+    job.save()
