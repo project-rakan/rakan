6691bfee2f1907ddb2edc0ab4603adb35d247637
Norton Pengra <npengra317@gmail.com>
2020-05-27 13:43:46 -0700

    tweak celery tests

--- bladecaller/api/tests/test_engine.py
+++ bladecaller/api/tests/test_engine.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from django.test.utils import override_settings
 from rest_framework.test import APITestCase
 
+import time
 
 # Create your tests here.
 
@@ -15,9 +17,15 @@ class EngineTests(APITestCase):
             precincts=4
         )
 
+    def tearDown(self):
+        from api.models import GeneratedMap
+        for map in GeneratedMap.objects.all():
+            map.delete()
+
     def test_get_startMap(self):
         response = self.client.get('/startjob', format='json')
         self.assertEqual(response.status_code, 405)
+
 
     def test_post_startMap_missing_id(self):
         response = self.client.post('/startjob', {}, format='json')
@@ -45,6 +53,7 @@ class EngineTests(APITestCase):
         self.assertTrue('missing' in response.data['msg'].lower())
         self.assertEqual(response.data['missing_field'], 'alpha')
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     def test_post_startMap_duplicate_job(self):
         from api.models import Job
         guid = '0'
@@ -80,7 +89,7 @@ class EngineTests(APITestCase):
 
         response = self.client.post('/startjob', {
             'id': guid,
-            'state': 'NOT A STATE :o',
+            'state': 'EX',
             'alpha': 'a',
             'beta': 0,
             'gamma': 0,
@@ -88,7 +97,9 @@ class EngineTests(APITestCase):
         }, format='json')
         self.assertEqual(response.status_code, 400)
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     def test_post_startMap_fires_signal(self):
+        from api.models import Job
         guid = '0'
         response = self.client.post('/startjob', {
             'id': guid,
@@ -99,11 +110,26 @@ class EngineTests(APITestCase):
             'eta': 0
         }, format='json')
         self.assertEqual(response.status_code, 201)
+        self.assertEqual(len(Job.objects.all()), 1)
+        self.assertEqual(Job.objects.all()[0].jobId, guid)
+        self.assertEqual(Job.objects.all()[0].state.state, 'EX')
+        self.assertEqual(Job.objects.all()[0].alpha, 0.)
+        self.assertEqual(Job.objects.all()[0].beta, 0.)
+        self.assertEqual(Job.objects.all()[0].gamma, 0.)
+        self.assertEqual(Job.objects.all()[0].eta, 0.)
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     def test_post_startMap_worker_role(self):
         from api.models import Job, GeneratedMap, State
         from api.tasks import performMetropolisHastingsWalk
         guid = '0'
-        job = Job.objects.create(jobId=guid, state=self.ex_state, steps=1, alpha=0.0, beta=0.0, gamma=0.0, eta=0.0)
-        performMetropolisHastingsWalk(guid)
-        self.assertEqual(len(GeneratedMap.objects.all()), 1)
+        job = Job.objects.create(jobId=guid, state=self.ex_state, steps=1, alpha=0.0, beta=0.0, gamma=0.0, eta=0.0, finished=True)
+        performMetropolisHastingsWalk(job.id)
+        self.assertEqual(len(Job.objects.all()), 1)
+        self.assertEqual(Job.objects.all()[0].jobId, guid)
+        self.assertEqual(Job.objects.all()[0].state.state, 'EX')
+        self.assertEqual(Job.objects.all()[0].alpha, 0.)
+        self.assertEqual(Job.objects.all()[0].beta, 0.)
+        self.assertEqual(Job.objects.all()[0].gamma, 0.)
+        self.assertEqual(Job.objects.all()[0].eta, 0.)
+        self.assertTrue(Job.objects.all()[0].finished)
