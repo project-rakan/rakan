5d873443c2466ea19c7b4d58208a3f5d231f125c
William Castro <castrowilliam616@gmail.com>
2020-05-27 22:49:41 +0000

    added .gitignore

--- rakan/cRakan/build/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: //home/project/rakan/cRakan/lib/googletest/googlemock
-# Build directory: //home/project/rakan/cRakan/build/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
