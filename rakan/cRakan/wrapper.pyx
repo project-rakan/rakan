ebabb92182498d256dc369ac74c829c7c3c619e0
kdmcc <kdmcc@uw.edu>
2020-05-28 14:10:18 -0700

    implented pyx file

--- rakan/cRakan/wrapper.pyx
+++ rakan/cRakan/wrapper.pyx
@@ -4,6 +4,7 @@ from cython.operator import dereference, preincrement, address
 
 from libcpp.vector cimport vector as cvector
 from libcpp.string cimport string as cstring
+from libc.stdint cimport uint32_t as cuint32_t
 
 from wrapper cimport Runner as cRunner
 
@@ -19,13 +20,27 @@ cdef class Engine:
         "Create an new engine with the initialization data in the jsonLocation"
 
         print('mock init')
-        # self._runner = new cRunner()
         
         with open(jsonLocation) as json_file:
             data = json.load(json_file)
 
-        self._districts = data['numNodes']
-        self._precincts = data['numDistricts']
+        self._districts = data['numDistricts']
+        self._precincts = data['numPrecincts']
+
+        self._runner = new cRunner(self._precincts, self._districts)
+        
+        # first pass to add all nodes
+        for prec in data['precincts']:
+            self._addNode(prec['precID'], prec['county'], prec['minPopulation'], prec['majPopulation'])
+        
+        #second pass to add all edges
+        edgesFrom = set()
+        for prec in data['precincts']:
+            for neighbor in prec['neighbors']:
+                if neighbor not in edgesFrom:
+                    self._addEdge(prec['precID'], neighbor)
+                
+            edgesFrom.add(prec['precID'])
 
     def __dealloc__(self):
         del self._runner
@@ -33,26 +48,33 @@ cdef class Engine:
     def _addNode(self, int id, int county, int minorityPopulation, int majorityPoplation):
         print('mock addNode')
         print(id, county, minorityPopulation, majorityPoplation)
+        dereference(self._runner).add_node(id, county, minorityPopulation, majorityPoplation);
 
     def _addEdge(self, int id1, int id2):
         print('mock addEdge')
+        dereference(self._runner).add_edge(id1, id2)
 
-    def setDistricts(self, cvector[int] districts):
+    def setDistricts(self, cvector[cuint32_t] districts):
         print('mock setDistricts')
+        dereference(self._runner).set_districts(districts)
 
     def getMaps(self):
         print('mock getMaps')
         return [[self._districts - 1] * self._precincts]
+        dereference(self._runner).getMaps()
 
     def getScores(self):
         print('mock getScores')
         return[{'compactness': 0.1, 'vra': 0.8, 'population':0.6, 'political': 0.76}]
+        dereference(self._runner).getScores()
 
     def seed(self):
         print('mock seed')
+        return dereference(self._runner).seed()
 
     def walk(self, int stepsToTake, double alpha, double beta, double gamma, double eta):
         print('mock walk')
+        return dereference(self._runner).Walk(stepsToTake, alpha, beta, gamma, eta)
 
     @property
     def districts(self):
