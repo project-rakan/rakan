314dfa815eaf30cf2d1f8f2b58c131ee13430edf
Idess Lee <idess@uw.edu>
2020-05-27 17:57:00 -0700

    Added 2 edge BFS test

--- rakan/cRakan/tst/test_runner.cc
+++ rakan/cRakan/tst/test_runner.cc
@@ -8,36 +8,67 @@
 
 namespace rakan {
 
-TEST(Test_Runner, TestBFS) {
+TEST(Test_Runner, TestBFSOneEdge) {
   unordered_set<uint32_t> unused;
   Graph *g;
-  Node *n1, *n2, *n3, *n4;
+  Node *n0, *n1;
 
-  unused.insert(1);
-  unused.insert(2);
-  unused.insert(3);
-  unused.insert(4);
+  for (uint32_t i = 1; i <= 4; i++) {
+    unused.insert(i);
+  }
 
-  // r1 has 2 precincts, n1 and n2. They're each other's neighbors.
+  // r1 has 2 precincts {n0 and n1}.
+  // n0 <-> n1
   Runner r1(2, 1);
+  r1.add_node(0, 1, 0, 0);
   r1.add_node(1, 1, 0, 0);
-  r1.add_node(2, 1, 0, 0);
-  r1.add_edge(1, 2);
+  r1.add_edge(0, 1);
+  n0 = r1.GetGraph()->GetNode(0);
   n1 = r1.GetGraph()->GetNode(1);
-  n2 = r1.GetGraph()->GetNode(2);
 
-  // Start BFS at n1, should find n2.
+  // Start BFS at n0, should find n1.
+  unused.erase(0);
+  ASSERT_EQ(r1.BFS(n0, &unused), n1);
+  unused.insert(0);
+
+  // Start BFS at n1, should find n0.
   unused.erase(1);
-  ASSERT_EQ(r1.BFS(n1, &unused), n2);
-  unused.insert(1);
-
-  // Start BFS at n2, should find n1.
-  unused.erase(2);
-  ASSERT_EQ(r1.BFS(n2, &unused), n1);
+  ASSERT_EQ(r1.BFS(n1, &unused), n0);
 
   // Erase both nodes from used. BFS should return nullptr.
-  unused.erase(1);
+  unused.clear();
+  ASSERT_EQ(r1.BFS(n0, &unused), nullptr);
   ASSERT_EQ(r1.BFS(n1, &unused), nullptr);
+}
+
+TEST(Test_Runner, TestBFSTwoEdges) {
+  unordered_set<uint32_t> unused;
+  Graph *g;
+  Node *n0, *n1, *n2;
+
+  for (uint32_t i = 0; i < 3; i++) {
+    unused.insert(i);
+  }
+
+  // r2 has 3 precincts {n0, n1, n2}.
+  // n0 <-> n1
+  // n1 <-> n2
+  Runner r2(3, 1);
+  r2.add_node(0, 1, 0, 0);
+  r2.add_node(1, 1, 0, 0);
+  r2.add_node(2, 1, 0, 0);
+  r2.add_edge(0, 1);
+  r2.add_edge(1, 2);
+
+  n0 = r2.GetGraph()->GetNode(0);
+  n1 = r2.GetGraph()->GetNode(1);
+  n2 = r2.GetGraph()->GetNode(2);
+  
+  // Start BFS at n1, should find n2, then n0.
+  unused.erase(1);
+  ASSERT_EQ(r2.BFS(n1, &unused), n2);
+  unused.erase(2);
+  ASSERT_EQ(r2.BFS(n1, &unused), n0);
 }
 
 }
