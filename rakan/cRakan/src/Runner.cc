e9a367bfdbec5b378494dbf295023d363bc68087
William Castro <castrowilliam616@gmail.com>
2020-05-28 01:20:32 +0000

    updated destructor and partial graph tests

--- rakan/cRakan/src/Runner.cc
+++ rakan/cRakan/src/Runner.cc
@@ -37,7 +37,7 @@ Runner::Runner() {
 }
 
 Runner::Runner(uint32_t num_precincts, uint32_t num_districts) {
-  graph_ = new Graph(num_precincts, num_districts, 0);
+  graph_ = new Graph(num_precincts, num_districts);
   changes_ = new unordered_map<int, int>;
   // each subsequent sub-vector will be initialized in the walk method.
   walk_changes_ = new vector<vector <uint32_t> *>;
@@ -62,8 +62,8 @@ void Runner::add_node(uint32_t node_id,
 }
 
 bool Runner::add_edge(uint32_t node_one, uint32_t node_two) {
-  if (graph_->ContainsNode(graph_->GetNode(node_one)) && 
-      graph_->ContainsNode(graph_->GetNode(node_two))) {
+  if (graph_->ContainsNode(graph_->GetNode(node_one)->id_) && 
+      graph_->ContainsNode(graph_->GetNode(node_two)->id_)) {
       graph_->AddEdge(node_one, node_two);
       return true;
   }
--- rakan/cRakan/tst/test_graph.cc
+++ rakan/cRakan/tst/test_graph.cc
@@ -10,41 +10,65 @@ namespace rakan {
 /* 
  * Constructor Checks
  */
+TEST(Test_Graph, TestConstructor) {
+  Graph g(1, 2);
+  ASSERT_EQ(g.GetNumNodes(), 1);
+  ASSERT_EQ(g.GetNumDistricts(), 2);
+  ASSERT_EQ(g.GetStatePop(), 0);
+}
 
-// Creates a graph with valid arguments.
-// Tests if creation was correct
-// TEST(Test_Graph, TestConstructor) {
-//   Graph g(1, 2, 3);
-//   ASSERT_EQ(g.GetNumNodes(), 1);
-//   ASSERT_EQ(g.GetNumDistricts(), 2);
-//   ASSERT_EQ(g.GetStatePop(), 3);
-// }
+/* 
+ * Nodes and Edges Checks
+ */
 
-// /* 
-//  * Nodes and Edges Checks
-//  */
+// Test simple add node by adding a single node.
+TEST(Test_Graph, TestAddSimple) {
+    // Check the graph state first
+    Graph g(1, 1);
+    ASSERT_EQ(g.AddNode(50, 1, 50, 30), false);
+    ASSERT_EQ(g.GetStatePop(), 0);
+    ASSERT_EQ(g.AddNode(0, 1, 40, 30), true);
+    ASSERT_EQ(g.GetStatePop(), 70);
+    ASSERT_EQ(g.GetNumNodes(), 1);
+    ASSERT_EQ(g.GetNumNodes(), 1);
+    ASSERT_EQ(g.ContainsNode(0), true);
+    ASSERT_EQ(g.ContainsNode(50), false);
 
-// // Test simple add node by adding a single node with no neighbors
-// TEST(Test_Graph, TestAddSimple) {
-//   Graph g(1, 1, 0);
-//   Node n(0);
-//   g.AddNode(&n);
+    Node* n = g.GetNode(0);
 
-//   ASSERT_EQ(g.GetNumNodes(), 1);
-//   ASSERT_EQ(g.GetNode(0), &n);
-// }
+    // Sanity check node in graph
+    ASSERT_EQ(n->GetCounty(), 1);
+    ASSERT_EQ(n->GetID(), 0);
+    ASSERT_EQ(n->GetTotalPop(), 70);
+    ASSERT_EQ(n->GetMajorityPop(), 40);
+    ASSERT_EQ(n->GetMinorityPop(), 30);
+}
 
-// // Test adding multiple nodes
-// TEST(Test_Graph, TestAddMultiple) {
-//   Graph g(2, 1, 0);
-//   Node n0(0);
-//   Node n1(1);
-//   g.AddNode(&n0);
-//   g.AddNode(&n1);
+// Check multiple adds then contains
+TEST(Test_Graph, TestMultipleContains) {
 
-//   ASSERT_EQ(g.GetNumNodes(), 2);
-//   ASSERT_EQ(g.GetNode(0), &n0);
-//   ASSERT_EQ(g.GetNode(1), &n1);
-// }
+}
 
+
+Graph *createSimpleGraph() {
+    Graph g(5, 3);
+    for (uint32_t i = 0; i < 5; i++) {
+        g.AddNode(i, i % 3, i, i);
+    }
+    for (int i = 1; i < 4; i++) {
+        g.AddEdge(i, i - 1);
+    }
+}
+// A helper method to create a graph
+Graph *createBigGraph() {
+    Graph g(60, 7);
+    for (uint32_t i = 0; i < 60; i++) {
+        g.AddNode(i, i % 10, i, i);
+    }
+    for (int i = 1; i < 59; i++) {
+        g.AddEdge(i, i - 1);
+        g.AddEdge(i, i + 1);
+    }
+
+}
 }   // namespace rakan
