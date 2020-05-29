7f627440f495a821f42614437358d40e6b3776bd
kdmcc <kdmcc@uw.edu>
2020-05-28 19:40:04 -0700

    creates the json files that the front end use to load up

--- bladecaller/datamerger/management/commands/create_frontend_json.py
+++ bladecaller/datamerger/management/commands/create_frontend_json.py
@@ -2,6 +2,7 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.core.management.base import BaseCommand
 from datamerger.models import VTDBlock, DistrictBlock, TractBlock
 from api.models import State
+from shapely.geometry import mapping
 
 from bladecaller.settings import DEBUG
 
@@ -12,6 +13,11 @@ import geopandas as gpd
 
 import os
 import io
+import json
+
+
+env = os.getenv("RAKAN_STATEFILES")
+OUTPUT_JSON_LOCATION = env + "/{code}.json"
 
 
 class Command(BaseCommand):
@@ -21,4 +27,64 @@ class Command(BaseCommand):
         parser.add_argument('--ignore-cache', type=bool, default=False)
 
     def handle(self, *args, **options):
-        raise NotImplementedError("Abstract!")
+        # raise NotImplementedError("Abstract!")
+        
+        djangoDF = pd.DataFrame(list(VTDBlock.objects.all().values()))
+        
+        for state in State.objects.all():
+            self.toJSON(djangoDF[djangoDF['state_id'] == state.id], state)
+
+    def toJSON(self, df1, state):
+
+        df = df1.reset_index()
+        # Convert each precinct's POLYGON into a list of (x,y) coordinates
+        stCode = state.state
+        maxDistricts = state.maxDistricts
+        fips = state.fips
+
+
+
+        coordLists = self.getPolyCoords(df.geometry)
+
+        
+        import pdb; pdb.set_trace()
+        precincts = []
+        for index, prec in df.iterrows():
+            precName = prec['name']
+            precID = index
+
+            vertices = []
+            for v in coordLists[index]:
+                coord = {
+                    "lat": float(v[1]),
+                    "lng": float(v[0])
+                }
+                vertices.append(coord)
+
+            precinctEntry = {
+                "name": precName,
+                "id": precID,
+                "vertices": vertices,
+            }
+            precincts.append(precinctEntry)
+
+        dictionary = {
+            "state": stCode,
+            "maxDistricts": maxDistricts,
+            "fips": fips,
+            "precincts": precincts
+        }
+
+        json_loc = OUTPUT_JSON_LOCATION.format(code=stCode)
+
+        with open(json_loc, "w") as outfile:
+            return outfile.write(json.dumps(dictionary, indent = 4))
+
+
+    def getPolyCoords(self, geo):
+        "Returns a tuple of x,y coords from a POLYGON in the form ((x1,y1),...,(xn,yn))"
+        coordsList = []
+        
+        for i in range(len(geo)):
+            coordsList.append(geo[i].coords[0])
+        return coordsList
