f63c5ecd460d7a65b41135704116fce20ec03ecf
Norton Pengra <npengra317@gmail.com>
2020-05-29 19:29:56 -0700

    change tests to use new pickles

--- bladecaller/api/tests/test_state.py
+++ bladecaller/api/tests/test_state.py
@@ -2,14 +2,15 @@ from django.test import TestCase
 from api.models import State
 
 import pickle
+import io
 
 class StateTests(TestCase):
     fixtures = ['iowa_washington_fixture.json']
 
     def test_generatesPayloadWithKeys(self):
         iowa = State.objects.get(fips=19)
-        handle = iowa.stateEngineData()
-        payload = pickle.loads(handle.read())
+        with io.open(iowa.stateEngineData, 'rb') as handle:
+            payload = pickle.loads(handle.read())
 
         self.assertTrue(isinstance(payload, dict))
         self.assertTrue('edges' in payload)
@@ -20,8 +21,8 @@ class StateTests(TestCase):
 
     def test_generatesPayloadWithCorrectStateValues(self):
         iowa = State.objects.get(fips=19)
-        handle = iowa.stateEngineData()
-        payload = pickle.loads(handle.read())
+        with io.open(iowa.stateEngineData, 'rb') as handle:
+            payload = pickle.loads(handle.read())
 
         self.assertEqual(payload['stCode'], iowa.state)
         self.assertEqual(payload['numPrecincts'], iowa.vtds.all().count())
