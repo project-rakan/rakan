12dee1bad2d995823fc87bb3d3c55d4408f13a2f
Norton Pengra <npengra317@gmail.com>
2020-05-30 15:59:02 -0700

    add task to visualize generated map

--- bladecaller/api/models.py
+++ bladecaller/api/models.py
@@ -6,6 +6,10 @@ from django.dispatch import receiver
 import os
 import io
 import pickle
+
+import geopandas as gpd
+from shapely import wkt
+
 from bladecaller.settings import MAP_ROOT
 
 # Create your models here.
@@ -31,6 +35,17 @@ class State(models.Model):
     @property
     def stateEngineData(self):
         return os.path.join(MAP_ROOT, f'{self.state}.pkl')
+
+    @property
+    def df(self):
+        table = {'geoid': [], 'geometry': [], 'district': [], 'engine_id': []}
+        for vtd in self.vtds.all():
+            table['geometry'].append(wkt.loads(vtd.geometry.wkt))
+            table['geoid'].append(vtd.geoid)
+            table['district'].append(vtd.district.district_id)
+            table['engine_id'].append(vtd.engine_id)
+
+        return gpd.GeoDataFrame(table).set_index('engine_id', drop=True)
         
 
 class GeneratedMap(models.Model):
@@ -48,6 +63,12 @@ class GeneratedMap(models.Model):
     vra = models.FloatField()
 
     visualization = models.ImageField()
+
+    @property
+    def df(self):
+        df = self.state.df
+        df['district'] = self.mapContents
+        return df
 
 class Job(models.Model):
     added = models.DateTimeField(auto_now_add=True)
@@ -68,6 +89,13 @@ class Job(models.Model):
     def __str__(self):
         return self.jobId
 
+@receiver(post_save, sender=GeneratedMap)
+def create_visualization(sender, **kwargs):
+    from api import tasks # to prevent circular imports
+    generatedMap = kwargs.get('instance')
+
+    if kwargs['created']:
+        tasks.visualizeMap.delay(generatedMap.id)
 
 @receiver(post_save, sender=Job)
 def queue_new_job(sender, **kwargs):
