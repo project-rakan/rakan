8ff8fdae51693b47f186c1fbd181b9721bd22ef7
Norton Pengra <npengra317@gmail.com>
2020-05-30 00:47:37 -0700

    register all models with admin page

--- bladecaller/datamerger/admin.py
+++ bladecaller/datamerger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from datamerger.models import VTDBlock, TractBlock, DistrictBlock
 
 # Register your models here.
+
+admin.site.register(VTDBlock)
+admin.site.register(TractBlock)
+admin.site.register(DistrictBlock)
