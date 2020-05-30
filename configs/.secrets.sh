2311eabb9f9197b6b009257591d724b9ff9fccd4
Norton Pengra <npengra317@gmail.com>
2020-05-30 16:22:45 -0700

    default to true until merge into master

--- configs/.secrets.sh
+++ configs/.secrets.sh
@@ -2,7 +2,7 @@
 
 # These are dummy values, change these values in production!
 
-export DEBUG_MODE=false
+export DEBUG_MODE=true
 export DATABASE_USER="default_user"
 export DATABASE_PASS="p@ssword1"
 export DATABASE_TABLE="table1"
