f7125e8ef17e379cddbdbec7530e360414b8b694
Norton Pengra <npengra317@gmail.com>
2020-05-31 16:51:17 -0700

    fix #59

--- bladecaller/api/tasks.py
+++ bladecaller/api/tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 from api.models import Job, GeneratedMap
 from rakan import Engine
 
+from django.db import IntegrityError
+
 import os
 import matplotlib.pyplot as plt
 from bladecaller.settings import RAKAN_STATE_VISUALIZATIONS
@@ -16,14 +18,21 @@ def performMetropolisHastingsWalk(jobId: int):
     scores = engine.getScores()
 
     # Generate the seed
-    GeneratedMap.objects.create(
-        state=job.state,
-        mapContents=maps[0],
-        compactness=scores[0][b'compact'],
-        vra=scores[0][b'vra'],
-        distribution=scores[0][b'distribution'],
-        borderRespect=scores[0][b'border'],
-    )
+    try:
+        mapModel = GeneratedMap.objects.create(
+            state=job.state,
+            mapContents=maps[0],
+            compactness=scores[0][b'compact'],
+            vra=scores[0][b'vra'],
+            distribution=scores[0][b'distribution'],
+            borderRespect=scores[0][b'border'],
+        )
+    except IntegrityError as e:
+        mapModel = GeneratedMap.objects.get(
+            mapContents=maps[0]
+        )
+    
+    job.generatedMaps.add(mapModel)
 
     engine.walk(job.steps, job.alpha, job.beta, job.gamma, job.eta)
 
@@ -31,15 +40,19 @@ def performMetropolisHastingsWalk(jobId: int):
     scores = engine.getScores()[1:]
 
     for map_, score_ in zip(maps, scores):
-        # Visualize the redistricting
-        mapModel = GeneratedMap.objects.create(
-            state=job.state,
-            mapContents=map_,
-            compactness=score_[b'compact'],
-            vra=score_[b'vra'],
-            distribution=score_[b'distribution'],
-            borderRespect=score_[b'border'],
-        )
+        try:
+            mapModel = GeneratedMap.objects.create(
+                state=job.state,
+                mapContents=map_,
+                compactness=score_[b'compact'],
+                vra=score_[b'vra'],
+                distribution=score_[b'distribution'],
+                borderRespect=score_[b'border'],
+            )
+        except IntegrityError:
+            mapModel = GeneratedMap.objects.get(
+                mapContents=map_
+            )
         job.generatedMaps.add(mapModel)
 
     job.finished = True
